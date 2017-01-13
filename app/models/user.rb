bdc29703e2cafdf1b902b09c74c93136b11e74cf
Valter Lima Mesquita <limavalter@gmail.com>
2017-01-12 21:58:23 -0300

    User signup created

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence:true, length: { minimum: 6 }
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -55,7 +55,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
-  test "password should be presente (nonblank)" do
+  test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
