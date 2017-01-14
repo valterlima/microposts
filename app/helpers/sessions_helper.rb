78aba8dd1e4d06553fa375564f4149972eaa1d35
Valter Lima Mesquita <limavalter@gmail.com>
2017-01-14 12:45:41 -0300

    New user actions added

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,10 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def current_user
     if @current_user.nil?
       @current_user = User.find_by(id: session[:user_id])
@@ -18,5 +22,18 @@ module SessionsHelper
   def log_out
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # Redirect to stored location (or to the default)
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
