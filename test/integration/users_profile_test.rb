6c8294339e7c79edf45f7b99eb1d2b60c054e9f9
Valter Lima Mesquita <limavalter@gmail.com>
2017-01-17 16:07:23 -0300

    Added user following and feed

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -6,27 +6,24 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
   def setup
-    @user = users(:michael)
-    @user2= users(:archer)
+    @michael = users(:michael)
+    @archer= users(:archer)
   end
 
   test "profile display" do
-    get user_path(@user)
+    get user_path(@michael)
     assert_template 'users/show'
-    assert_select 'title', full_title(@user.name)
-    assert_select 'h1', text: @user.name
+    assert_select 'title', full_title(@michael.name)
+    assert_select 'h1', text: @michael.name
     assert_select 'h1>img.gravatar'
-    assert_match @user.microposts.count.to_s, response.body
+    assert_match @michael.microposts.count.to_s, response.body
 
-    # @user.follow(@user2)
-    # @user2.follow(@user)
-
-    # assert @user.following?(@user2)
-    # assert_select '#following', @user.following.count
-    # assert_select '#followers', @user.followers.count
+    assert @archer.following?(@michael)
+    assert_select '#following', "#{@michael.following.count}"
+    assert_select '#followers', "#{@michael.followers.count}"
 
     assert_select 'div.pagination', count: 1
-    @user.microposts.paginate(page: 1).each do |micropost|
+    @michael.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
   end
